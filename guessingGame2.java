ad62ec513bf058175aab8aca64e2f5eeca9289a0
lex <lex@trabajar>
2017-03-02 18:00:08 -0500

    Random Java Code

---
+++ guessingGame2.java
@@ -0,0 +1,302 @@
+// import java.util.Scanner;
+// import java.util.Random;
+import java.util.*;
+
+public class guessingGame2 {
+    
+    private String playerName;
+    private int numberOfTries;
+    private int numberOfWins;
+    private int numberOfGamesPlayed;
+    private int totalSumeOfGuesses;
+    private boolean activeStatus;
+    
+    // Set Default Values
+    public guessingGame2 () {
+        this.playerName = "Jane Doe";
+        this.numberOfTries = 0;
+        this.numberOfWins = 0;
+        this.numberOfGamesPlayed = 0;
+        this.totalSumeOfGuesses = 0;
+        this.activeStatus = true;
+    }
+    
+    public guessingGame2 ( String playerName ) {
+        this.playerName = playerName;
+    }
+    
+    //  *** GET VALUES ** //
+    
+    public String getName  (){
+        return this.playerName;
+    }
+    
+    public int getNumberOfTries  (){
+        return this.numberOfTries;
+    }
+    
+    public int getNumberOfWins  (){
+        return this.numberOfWins;
+    }
+    
+    public int getAttemptStats  (){
+        
+        if ( ( this.totalSumeOfGuesses == 0) || ( this.numberOfGamesPlayed == 0) ) {
+            return 0;
+        }else {
+            return this.totalSumeOfGuesses/this.numberOfGamesPlayed;
+        }
+        
+    }
+    
+    //  *** SET VALUES ** //
+    
+    public void setName ( String setPlayerName ){
+        this.playerName = setPlayerName;   
+    }
+    
+    //public void setNumberOfTries ( int setNumberOfTries ){
+    public void setNumberOfTries ( ){
+        this.numberOfTries += 1;
+    }
+    
+    //public void setNumberOfWins ( int setNumberOfWins ){
+    public void setNumberOfWins ( ){
+        this.numberOfWins += 1;
+    }
+    
+    //public void setNumberOfGamesPlayed ( int setNumberOfGamesPlayed ){
+    public void setNumberOfGamesPlayed ( ){
+        this.numberOfGamesPlayed += 1;
+    }    
+    
+    // Set total number of tries
+    public void setTotalSumeOfGuesses ( ){
+        this.totalSumeOfGuesses += this.numberOfTries;
+    }   
+    
+    public void resetStatus ( ){
+        this.numberOfTries = 0;
+        this.numberOfWins = 0;
+        this.activeStatus = true;
+    }
+    
+    public void resetNumberOfTries ( ){
+        this.numberOfTries = 0;
+    }
+    
+    public static void printStatus ( guessingGame2 t ){
+        System.out.printf ("Name: %s # Tries: %d%n", t.playerName, t.numberOfTries );
+    }
+    
+    // Begin Game Logic
+    
+    // Add Player
+    
+    public static void addPlayer ( String newPlayerName , List <guessingGame2> listOfPlayers ){          
+        
+        listOfPlayers.add ( new guessingGame2 ( newPlayerName )  );
+        
+    }
+    
+    public static void gameGreeting ( String whatShouldIdisplay ){      
+        
+        switch ( whatShouldIdisplay ){
+            case "1" : 
+                System.out.printf ("%s%n", "This is a commandline guessing game." );
+                break;
+            case "2" : 
+                System.out.printf ("%s%n", "Choose a random number between 1 - X" );
+                break;
+        }
+    }
+    
+    
+    public static int initGame ( ){ 
+        Scanner in = new Scanner(System.in);
+        int upperLimitNumber = 0;
+        String input = "";
+                
+        while ( true ) {
+            System.out.printf ("%s%n", "Choose a number for the upper limits of guessing." );
+            input = in.next();
+            
+            try {
+                upperLimitNumber = Integer.parseInt ( input );
+                break;
+            } catch ( NumberFormatException ne) {
+                System.out.printf ( "%s%n", "\""+ input + "\", is not a number try again." );
+            }
+            
+        }
+        
+        return upperLimitNumber;
+    }
+    
+    public static String initGameGetFirstPlayerName ( ){ 
+       Scanner in = new Scanner(System.in);
+
+       System.out.printf ("%s%n", "Choose a new player name." );
+       String input = in.next();
+       System.out.printf ("%s%n", "Your new player's name is \"" + input + "\"." );
+       return input;
+    }
+    
+    // Delete player
+    public static void deletePlayer ( List <guessingGame2> listOfPlayers ){ 
+        
+        Scanner in = new Scanner(System.in);
+        
+        boolean shouldIloop = true;
+        
+        System.out.printf ("%s%n", "Type the name of the player you would like to delete.  \nThis is case sensative."  );
+        for ( int i = 0; i < listOfPlayers.size (); i++ ) {
+            System.out.printf ("Player : %s%n", listOfPlayers.get( i ).getName()  );
+        }
+        
+        while ( shouldIloop ) {
+            
+            String input = in.next();
+
+            for ( int i = 0; i < listOfPlayers.size (); i++ ) {
+
+                    if ( input.equals ( listOfPlayers.get( i ).getName() ) ){
+                        System.out.printf ("Removing Player : %s%n", listOfPlayers.get( i ).getName() );
+                        listOfPlayers.remove ( i );
+                        
+                        // Display how many player(s) left
+                        for ( int ii = 0; ii < listOfPlayers.size (); ii++ ) {
+                            System.out.printf ("Remaining Player(s) : %s%n", listOfPlayers.get( ii ).getName()  );
+                        }
+                        
+                        shouldIloop = false;
+                    } else if ( (shouldIloop == true) && (i == listOfPlayers.size ()-1) ) {
+                        System.out.printf ("%s%n", "Player " + input+ " does not exist." );
+                        System.out.printf ("%s%n", "Type the name of the player you would like to delete?"  );
+                        for ( int ii = 0; ii < listOfPlayers.size (); ii++ ) {
+                            System.out.printf ("Player : %s%n", listOfPlayers.get( ii ).getName()  );
+                        }
+                    }
+            }
+            
+        }
+        
+    }
+    
+    public static void displayStats ( List <guessingGame2> listOfPlayers ){ 
+        
+            for ( int i = 0; i < listOfPlayers.size (); i++ ) {
+                System.out.printf ("%10s %10s %s %10d %s %10d%n", "Player :: ", listOfPlayers.get( i ).getName(), " Number Of Wins :", listOfPlayers.get( i ).getNumberOfWins(), " Average Guesses :", listOfPlayers.get( i ).getAttemptStats() );
+            }
+    }
+    
+    public static void incrementAllPlayersNumberOfGamesPlayed ( List <guessingGame2> listOfPlayers ){ 
+        
+            for ( int i = 0; i < listOfPlayers.size (); i++ ) {
+               listOfPlayers.get( i ).setNumberOfGamesPlayed();
+            }
+    }
+    
+    public static void startPlaying ( List <guessingGame2> listOfPlayers ){
+        
+        Random random =  new Random();
+        
+        // Set number for random shuffle
+        int numberToRandomize = initGame ( );
+        
+        // Randomize the starting number
+        int randomNumber = random.nextInt( numberToRandomize )+1;
+        
+        // Set default user
+        addPlayer ( initGameGetFirstPlayerName () ,  listOfPlayers);
+        
+        int whoseTurnIsIt = -1;
+        System.out.printf (" randomNumber :: %d%n%n", randomNumber );
+        
+        Scanner in = new Scanner(System.in);
+        boolean shouldIloop = true;
+        while ( shouldIloop ) {
+            
+            // Choose the next player
+            if ( whoseTurnIsIt < listOfPlayers.size ()-1 ){
+                whoseTurnIsIt +=1;
+            } else{
+                whoseTurnIsIt = 0;
+            }
+            
+            System.out.printf ("%n\"%s\", I am thinking of a number between 0 and %d.  Can you guess what it is?\n%n", listOfPlayers.get( whoseTurnIsIt ).getName() , numberToRandomize);
+            System.out.printf ("%s%n","[q]uit | [a]dd player | [d]elete player | [g]et stats");
+            String input = in.next();
+            
+            // System.out.printf ("%s%n","[\033[1m q \033[0m]uit | [\033[1m a \033[0m]dd | [\033[1m d \033[0m]elete | [\033[1m g \033[0m] et stats");
+                        
+            try {
+                Integer.parseInt ( input );
+                
+               // Help the user know if they are guessing in the correct order
+               if ( Integer.valueOf (input) < randomNumber ) {
+                   // Add to the number of guesses
+                   listOfPlayers.get( whoseTurnIsIt ).setNumberOfTries();
+                   
+                   System.out.printf ("You guessed %d, try guessing higher\n", Integer.valueOf (input));
+               } else if ( Integer.valueOf (input) > randomNumber ) {
+                   // Add to the number of guesses
+                   listOfPlayers.get( whoseTurnIsIt ).setNumberOfTries();                
+                   System.out.printf ( "You guessed %d, try guessing lower\n", Integer.valueOf (input) );
+               } else if ( Integer.valueOf (input) == randomNumber ) {
+                   
+                   // Add to the number of guesses
+                   listOfPlayers.get( whoseTurnIsIt ).setNumberOfTries();
+                   
+                   // If equal add a win to the users stats
+                   listOfPlayers.get( whoseTurnIsIt ).setNumberOfWins();
+                   
+                   // Increment the number of games played for each player
+                   incrementAllPlayersNumberOfGamesPlayed ( listOfPlayers );
+                       
+                   System.out.printf ("\"%s\", you guessed %d, using %d guesses! You have %d wins.%n", listOfPlayers.get( whoseTurnIsIt ).getName(), Integer.valueOf (input), listOfPlayers.get( whoseTurnIsIt ).getNumberOfTries (), listOfPlayers.get( whoseTurnIsIt ).getNumberOfWins () );
+                   
+                   //Change the random number
+                   randomNumber = random.nextInt( numberToRandomize )+1;
+                   System.out.printf ("randomNumber :: %d%n", randomNumber );
+                   
+                   // Add sum of total tries                   
+                   listOfPlayers.get( whoseTurnIsIt ).setTotalSumeOfGuesses();
+                   
+                   // Reset number of tries
+                   listOfPlayers.get( whoseTurnIsIt ).resetNumberOfTries();
+               }
+                
+            } catch ( NumberFormatException ne) {
+ 
+                if ( input.equals("q")){
+                    shouldIloop = false;
+                }else if ( input.equals("a") ) {
+                    addPlayer ( initGameGetFirstPlayerName () ,  listOfPlayers);
+                }else if ( input.equals("d") ) {
+                    // Check if there is less than 1 player.  If yes you can not have zero sum players
+                    if ( listOfPlayers.size() <2 ){
+                        System.out.printf ("%n%s%n%n", "You can not delete the only player." );
+                    }else{
+                        deletePlayer ( listOfPlayers );
+                    }
+                 }else if ( input.equals("g") ) {
+                     displayStats ( listOfPlayers );
+                }
+            }
+                        
+        }
+    }
+
+    // End Game Logic
+    
+    public static void main(String[] args){
+        
+           List<guessingGame2> listOfPlayers = new ArrayList<guessingGame2>();
+           
+           startPlaying ( listOfPlayers );        
+    }
+    
+
+    
+}
